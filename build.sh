e79f2d11cb49f89591aa34464de5ae82471f5bfd
Vinh Vo <vdvinh@gmail.com>
2017-04-02 11:34:32 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,1 @@
-docker build -t vinhhrv/android_ci .
-docker tag vinhhrv/android_ci vinhhrv/android_ci:latest
-docker tag vinhhrv/android_ci vinhhrv/android_ci:$1
-docker push vinhhrv/android_ci:latest
-docker push vinhhrv/android_ci:$1
+docker build -t android_ci .
